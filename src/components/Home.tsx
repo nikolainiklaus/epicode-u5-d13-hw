7c15bfc1cb23ecda6e24414fa748c37ca71c691b
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-04-13 15:11:19 +0200

    Typing indication

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -6,109 +6,146 @@ import {
   Form,
   FormControl,
   ListGroup,
-} from "react-bootstrap"
-import { io } from 'socket.io-client'
-import { User, Message } from '../types'
+} from "react-bootstrap";
+import { io } from "socket.io-client";
+import { User, Message } from "../types";
+import { ChangeEvent } from "react";
 
-
-
-const socket = io("http://localhost:3001", { transports: ['websocket'] }) 
+const socket = io("http://localhost:3001", { transports: ["websocket"] });
 
 const Home = () => {
-  const [username, setUsername] = useState("")
-  const [message, setMessage] = useState("")
-  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [chatHistory, setChatHistory] = useState<Message[]>([])
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [chatHistory, setChatHistory] = useState<Message[]>([]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [typingUsers, setTypingUsers] = useState("");
 
   useEffect(() => {
- 
-    socket.on("welcome", welcomeMessage => {
-      console.log(welcomeMessage)
+    socket.on("welcome", (welcomeMessage) => {
+      console.log(welcomeMessage);
 
-      socket.on("loggedIn", onlineUsersList => {
-        console.log(onlineUsersList)
-        setOnlineUsers(onlineUsersList)
-        setLoggedIn(true)
-      })
+      socket.on("loggedIn", (onlineUsersList) => {
+        console.log(onlineUsersList);
+        setOnlineUsers(onlineUsersList);
+        setLoggedIn(true);
+      });
 
-      socket.on("updateOnlineUsersList", updatedList => {
-        setOnlineUsers(updatedList)
-      })
+      socket.on("updateOnlineUsersList", (updatedList) => {
+        setOnlineUsers(updatedList);
+      });
 
-      socket.on("newMessage", newMessage => {
-        console.log(newMessage)
-     
-        setChatHistory((chatHistory) => [...chatHistory, newMessage.message])
-      })
-    })
-  }, [])
+      socket.on("newMessage", (newMessage) => {
+        console.log(newMessage);
+
+        setChatHistory((chatHistory) => [...chatHistory, newMessage.message]);
+      });
+    });
+  }, []);
+
+  socket.on("typing", ({ username }) => {
+    console.log(`${username} is typing...`);
+    setIsTyping(true);
+    setTypingUsers(username);
+  });
+
+  socket.on("stoppedTyping", () => {
+    console.log("Stopped typing.");
+    setIsTyping(false);
+  });
 
   const submitUsername = () => {
-
-    socket.emit("setUsername", { username })
-  }
+    socket.emit("setUsername", { username });
+  };
 
   const sendMessage = () => {
     const newMessage = {
       sender: username,
       text: message,
-      createdAt: new Date().toLocaleString("en-US")
+      createdAt: new Date().toLocaleString("en-US"),
+    };
+    socket.emit("sendMessage", { message: newMessage });
+    setChatHistory([...chatHistory, newMessage]);
+    setMessage("");
+    setIsTyping(false);
+    setTypingUsers("");
+    socket.emit("stoppedTyping");
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+    if (e.target.value) {
+      socket.emit("typing", { username });
+    } else {
+      socket.emit("stoppedTyping");
     }
-    socket.emit("sendMessage", { message: newMessage })
-    setChatHistory([...chatHistory, newMessage])
-  }
+  };
 
   return (
     <Container fluid>
       <Row style={{ height: "95vh" }} className="my-3">
         <Col md={9} className="d-flex flex-column justify-content-between">
-    
+          {/* LEFT COLUMN */}
+          {/* TOP AREA: USERNAME INPUT FIELD */}
+          {/* {!loggedIn && ( */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
-              submitUsername()
+            onSubmit={(e) => {
+              e.preventDefault();
+              submitUsername();
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
-              onChange={e => setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
               disabled={loggedIn}
             />
           </Form>
-
+          {/* )} */}
+          {/* MIDDLE AREA: CHAT HISTORY */}
           <ListGroup>
-            {chatHistory.map((message, index) => (<ListGroup.Item key={index}>{<strong>{message.sender} </strong>} | {message.text} at {message.createdAt}</ListGroup.Item>))}
+            {chatHistory.map((message, index) => (
+              <ListGroup.Item key={index}>
+                {<strong>{message.sender} </strong>} | {message.text} at{" "}
+                {message.createdAt}
+              </ListGroup.Item>
+            ))}
           </ListGroup>
-
+          {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
-              sendMessage()
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendMessage();
             }}
           >
             <FormControl
               placeholder="Write your message here"
               value={message}
-              onChange={e => setMessage(e.target.value)}
+              onChange={handleInputChange}
               disabled={!loggedIn}
             />
+            {isTyping && typingUsers !== username && (
+              <ListGroup.Item>{`${typingUsers} is typing...`}</ListGroup.Item>
+            )}
           </Form>
         </Col>
         <Col md={3}>
-
+          {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
           {onlineUsers.length === 0 && (
             <ListGroup.Item>Log in to check who's online!</ListGroup.Item>
           )}
           <ListGroup>
-            {onlineUsers.map(user => (<ListGroup.Item key={user.socketId}>{user.username}</ListGroup.Item>))}
+            {onlineUsers.map((user) => (
+              <ListGroup.Item key={user.socketId}>
+                {user.username}
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         </Col>
       </Row>
     </Container>
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
